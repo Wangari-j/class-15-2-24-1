dcdb594bd9887ef0e276c3d431d9e182b4d40de4
Wangari-j <wangarijudy@gmail.com>
2024-02-16 15:35:20 +0300

    mount

--- classes.py
+++ classes.py
@@ -5,9 +5,13 @@ class Mountains:
     def moExample(self):
         print("Mt.Kenya and Mt. Kilimanjaro are volcano mountains in Africa.")
         
+    def __init__(self,volcano,state):
+        self.volcano = volcano
+        self.state = state
 
-mountains1 = Mountains()
-mountains2 = Mountains()
+
+mountains1 = Mountains("kenya", "dormant")
+mountains2 = Mountains("volcano","state")
 
 mountains1.volcano = "some mountains are volcano"
 mountains1.state = "Most are inactive"
